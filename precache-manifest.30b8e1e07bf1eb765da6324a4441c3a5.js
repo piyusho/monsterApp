5ed34d435967e02ab2f86e3fa2671a66351dc4df
piyush-mahapatra-au6 <piyushmahapatra001@gmail.com>
2020-06-10 22:56:28 +0530

    Updates

---
+++ precache-manifest.30b8e1e07bf1eb765da6324a4441c3a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6097f3c8827b903f046e0abfb95df76d",
+    "url": "/monsterApp/index.html"
+  },
+  {
+    "revision": "af93648ca9bf4d23b64c",
+    "url": "/monsterApp/static/css/main.60bde1cf.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsterApp/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterApp/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af93648ca9bf4d23b64c",
+    "url": "/monsterApp/static/js/main.7c9fa9f9.chunk.js"
+  },
+  {
+    "revision": "398d4a0efe4e45307e54",
+    "url": "/monsterApp/static/js/runtime-main.c446ef10.js"
+  }
+]);
